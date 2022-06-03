556cc07e829dc255569c35239341de5b045fd225
adanfj <ajffj4@gmail.com>
2022-06-03 20:24:51 +0200

    API & Pages setup

---
+++ pages/selector.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContentSelector = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ContentSelector
---
+++ pages/viewer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContentViewer = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ContentViewer
