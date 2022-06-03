9fb9db23463ddbba189391cdd63b2220d1edc23d
adanfj <ajffj4@gmail.com>
2022-06-03 22:01:13 +0200

    run commands in shell

--- pages/viewer.jsx
+++ pages/viewer.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const ContentViewer = () => {
   return (
-    <div></div>
+    <main>
+        <div></div>
+        <div className="content"></div>
+    </main>
   )
 }
 
 export default ContentViewer
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: 'Content Viewer',
+      description: 'Content Viewer',
+      keywords: 'Content Viewer',
+    },
+  }
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: 'Content Viewer',
+      description: 'Content Viewer',
+      keywords: 'Content Viewer',
+    },
+  }
+}
